617028a8141dc605a5e02650e89026d69e7c9302
Govindarajan Vishnuchithan <govindarajanv@gmail.com>
2018-03-31 07:45:33 +0530

    terraform template to add a basic vpc

---
+++ create_basic_vpc/create_basic_vpc.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+region = "ap-southeast-2"
+access_key = "Your Access Key"
+secret_key = "Your Secret Key"
+}
+
+resource "aws_vpc" "terraform-vpc" {
+cidr_block = "10.0.0.0/16"
+instance_tenancy = "default"
+enable_dns_support = "true"
+enable_dns_hostnames = "true"
+enable_classiclink = "false"
+	tags {
+		Name = "terraform"
+	}
+}
+
+resource "aws_subnet" "public-1" {
+vpc_id = "${aws_vpc.terraform-vpc.id}"
+cidr_block ="10.0.1.0/24"
+map_public_ip_on_launch = "true"
+availability_zone = "ap-southeast-2b"
+tags {
+Name = "public"
+}
+}
+
+resource "aws_subnet" "private-1" {
+vpc_id = "${aws_vpc.terraform-vpc.id}"
+cidr_block ="10.0.100.0/24"
+map_public_ip_on_launch = "false"
+availability_zone = "ap-southeast-2b"
+tags {
+Name = "private"
+}
+}
