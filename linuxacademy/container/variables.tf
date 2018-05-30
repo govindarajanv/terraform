28d3c8f3f3c48236f74fd9ddf8f5e8b84a22e4f0
root <govindarajanv@gmail.com>
2018-05-30 07:36:45 +0000

    terraform exercises

---
+++ linuxacademy/container/variables.tf
@@ -0,0 +1,13 @@
+variable "container_name" {
+  description = "Name of container"
+  default = "blog"
+}
+variable "int_port" {
+  description = "internal port for container"
+  default = "2368"
+}
+variable "ext_port" {
+  description = "external port for container"
+  default = "8128"
+}
+variable "image" {}
