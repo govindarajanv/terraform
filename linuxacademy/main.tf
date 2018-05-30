fe948c4734d2ec8b9d378841a7fda5a0b7e56354
root <govindarajanv@gmail.com>
2018-05-30 07:14:06 +0000

    terraform exercises

--- linuxacademy/main.tf
+++ linuxacademy/main.tf
@@ -1,20 +1,3 @@
-variable "container_name" {
-  description = "Name of container"
-  default = "blog"
-}
-variable "image" {
-  description = "image for container"
-  default = "ghost:latest"
-}
-variable "int_port" {
-  description = "internal port for container"
-  default = "2368"
-}
-variable "ext_port" {
-  description = "external port for container"
-  default = "8128"
-}
-
 # Download the latest Ghost Image
 resource "docker_image" "image_id" {
   name = "${var.image}"
@@ -27,13 +10,4 @@ resource "docker_container" "container_id" {
     internal = "${var.int_port}"
     external = "${var.ext_port}"
   }
-}
-
-#Output the IP Address of the Container
-output "IP Address" {
-  value = "${docker_container.container_id.ip_address}"
-}
-
-output "container_name" {
-  value = "${docker_container.container_id.name}"
 }
