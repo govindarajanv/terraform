f93e1d7ee01a626df37e33cc39c11970a9d7463a
root <govindarajanv@gmail.com>
2018-05-31 17:34:31 +0000

    terraform exercises

--- linuxacademy/main.tf
+++ linuxacademy/main.tf
@@ -13,3 +13,9 @@ module "container" {
   int_port = "${var.int_port}"
   ext_port = "${lookup(var.ext_port,var.env)}"
 }
+
+resource "null_resource" "null_id" {
+  provisioner "local-exec" {
+    command = "echo ${module.container.container_name}:${module.container.ip_addr} >> container.txt"
+  }
+}
