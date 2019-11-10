3112345831e13dbca6f0430ada1c750b7cba5975
Govindarajan V <govindarajanv@gmail.com>
2019-11-10 09:58:32 +0000

    added output and variables

--- tuar/practice/main.tf
+++ tuar/practice/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "example" {
-  ami           = "ami-0bba96c31d87e65d9"
+  ami           = "ami-04b9e92b5572fa0d1"
   instance_type = "t2.micro"
   subnet_id = "subnet-04b5df50da57b4bd3"
   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
@@ -11,7 +11,7 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
               	#!/bin/bash
               	echo "Hello, World" > index.html
-              	nohup busybox httpd -f -p 8080 &
+              	nohup busybox httpd -f -p ${var.server_port}  &
               	EOF
 
   tags = {
@@ -23,10 +23,19 @@ resource "aws_security_group" "instance" {
   vpc_id = "vpc-020c6a047e111d404"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+}
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
 }
